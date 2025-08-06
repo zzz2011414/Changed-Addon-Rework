5c7f6bb9315c49f4037a3d0b45123e77e0933a6c
Foxyas <CarlosDeck9474@gmail.com>
2025-08-06 14:59:24 -0300

    tweaks

--- src/main/java/net/foxyas/changedaddon/abilities/CustomInteraction.java
+++ src/main/java/net/foxyas/changedaddon/abilities/CustomInteraction.java
@@ -10,6 +10,10 @@ import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.resources.ResourceLocation;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 public class CustomInteraction extends AbstractAbility<CustomInteractionInstance> {
 
     public CustomInteraction(){
@@ -32,6 +36,19 @@ public class CustomInteraction extends AbstractAbility<CustomInteractionInstance
         }
 
         return super.getCoolDown(entity);
+    }
+
+    @Override
+    public Collection<Component> getAbilityDescription(IAbstractChangedEntity entity) {
+        if (entity.getChangedEntity() instanceof LatexSnepEntity || entity.getChangedEntity() instanceof AbstractLuminarcticLeopard){
+            if (entity.getChangedEntity() instanceof LatexSnepEntity) {
+                return List.of(new TranslatableComponent("changed_addon.ability.custom_interaction.have_interaction.latex_snep"));
+            } else {
+                return List.of(new TranslatableComponent("changed_addon.ability.custom_interaction.have_interaction.luminarctic_leopards"));
+            }
+
+        }
+        return super.getAbilityDescription(entity);
     }
 
     @Nullable
